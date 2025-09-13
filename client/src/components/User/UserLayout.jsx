4a45ca5350b5b43d8fd6835c6dd12d05ed71772c
Zookegger <nguyenductrung11122004@gmail.com>
2025-09-13 11:22:04 +0700

    feat: Add react-resizable-panels for adjustable layout in UserLayout; refactor Sidebar component for improved styling

--- client/src/components/User/UserLayout.jsx
+++ client/src/components/User/UserLayout.jsx
@@ -3,19 +3,32 @@ import { UserFooter as Footer } from "./UserFooter";
 import Sidebar from "./UserSidebar";
 import "./UserLayout.css";
 import { Outlet } from "react-router-dom";
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
+import { Box } from "@mui/material";
 
 export const UserLayout = ({ children, user }) => {
 	return (
-		<div className="layout-container">
+		<Box className="layout-container">
 			{/* <Header /> */}
-			<div className="layout-body">
-				<Sidebar />
-				<main className="main-content">
-					{/* Render children if provided, otherwise render Outlet for nested routes */}
-					{children || <Outlet />}
-				</main>
-			</div>
+			<PanelGroup
+				className="layout-body"
+				direction="horizontal"
+				style={{ flex: 1 }}
+			>
+				<Panel minSize={10} defaultSize={25} order={1}>
+					<Sidebar />
+				</Panel>
+				<PanelResizeHandle
+					style={{ width: "0.25rem", backgroundColor: "#000000" }}
+				/>
+				<Panel order={2} minSize={65}>
+					<Box component="main" className="main-content">
+						{/* Render children if provided, otherwise render Outlet for nested routes */}
+						{children || <Outlet />}
+					</Box>
+				</Panel>
+			</PanelGroup>
 			{/* <Footer /> */}
-		</div>
+		</Box>
 	);
 };
--- client/src/components/User/UserSidebar.jsx
+++ client/src/components/User/UserSidebar.jsx
@@ -12,15 +12,15 @@ import {
 
 export default function Sidebar() {
 	return (
-		<Box sx={{ boxShadow: 2 }}>
-			<List>
+		<Box>
+			<List sx>
 				{[...Array(5)].map((_, i) => (
 					<ListItemButton key={i} sx={{ px: 3 }}>
 						Sidebar {i + 1}
 					</ListItemButton>
 				))}
 
-                {/* Get user's chat sessions */}
+				{/* Get user's chat sessions */}
 			</List>
 		</Box>
 	);
